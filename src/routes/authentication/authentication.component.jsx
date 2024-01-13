94d7d02f48023a109be8dc37a73b1d3fb4c1d698
davidlerner26 <david.lerner@hotmail.com>
2024-01-13 17:01:48 -0300

    Clean code

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,19 +1,12 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import {
-  createUserDocumentFromAuth,
-  signInWithGooglePopup,
-} from "../../utils/firebase/firebase.util";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.styles.scss";
 
 const Authentication = () => {
-  const logGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
-  };
-
   return (
-    <div>
-      <h1>Sign In Page</h1>
-      <button onClick={logGoogle}>Sign In with Google Popup</button>
+    <div className="authentication-container">
+      <SignInForm />
       <SignUpForm />
     </div>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment } from "react";
-import { Link, Outlet } from "react-router-dom";
-import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
 import "./navigation.styles.scss";
 
 const Navigation = () => {
@@ -8,7 +10,7 @@ const Navigation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrownLogo className="logo" />
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
