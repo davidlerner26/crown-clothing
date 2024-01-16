4b8bd373db38c0f704f410eb8d246930daab0dc8
davidlerner26 <david.lerner@hotmail.com>
2024-01-16 20:25:18 -0300

    Store products in firebase and create category page

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/user.context";
-import { ProductsProvider } from "./context/products.context";
 import { CartProvider } from "./context/cart.context";
+import { CategoriesProvider } from "./context/categories.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -14,11 +14,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
