87ffaf7dcbd1185449d09e5cd3658c4fccbb96de
davidlerner26 <david.lerner@hotmail.com>
2024-01-18 09:59:07 -0300

    Fix clashing styles

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,11 @@
-import './category-item.styles.scss';
+import './directory-item.styles.scss';
 
-const CategoryItem = ({ category }) => {
+const DirectoryItem = ({ category }) => {
     const { id, title, imageUrl } = category;
     return (
         <div key={id} className="category-container" style={{ backgroundImage: `url(${imageUrl})` }}>
             <div className="background-image" />
-            <div className="category-body-container">
+            <div className="body">
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </div>
@@ -13,4 +13,4 @@ const CategoryItem = ({ category }) => {
     )
 }
 
-export default CategoryItem;
+export default DirectoryItem;
