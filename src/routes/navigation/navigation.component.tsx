3dc4ff00315305996849bcff0cecc8668b5faf53
David Lerner <david.lerner@hotmail.com>
2026-04-20 15:30:08 -0300

    Align logo with navbar items

--- src/routes/navigation/navigation.component.tsx
+++ src/routes/navigation/navigation.component.tsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
@@ -19,7 +19,6 @@ import { ClickOutside } from '../../components/cick-outside/cick-outside.compone
 import ErrorMessage from '../../components/error-message/error-message.component';
 import { setIsCartOpen } from '../../store/cart/cart.action';
 import {
-  LogoContainer,
   NavigationContainer,
   NavLink,
   NavLinkButton,
@@ -56,9 +55,9 @@ const Navigation = () => {
   return (
     <>
       <NavigationContainer>
-        <LogoContainer to="/">
+        <Link to="/">
           <img src={CrownLogo} alt="Crown logo" />
-        </LogoContainer>
+        </Link>
         <ErrorMessage errorMessage={errorMessage} />
         <NavLinks>
           {items.map((item) => {
--- src/routes/navigation/navigation.styles.tsx
+++ src/routes/navigation/navigation.styles.tsx
@@ -10,23 +10,13 @@ export const NavigationContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+  align-items: center;
   margin-bottom: 25px;
 
   @media screen and (max-width: 800px) {
     height: 60px;
     padding: 10px 20px;
     margin-bottom: 20px;
-  }
-`;
-
-export const LogoContainer = styled(Link)`
-  height: 100%;
-  width: 70px;
-  padding: 25px;
-
-  @media screen and (max-width: 800px) {
-    width: 50px;
-    padding: 0;
   }
 `;
 
