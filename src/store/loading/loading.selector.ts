62690feeb220dcf57520912353880016438351dc
David Lerner <david.lerner@hotmail.com>
2026-04-20 15:56:25 -0300

    Remove loading reducer

--- src/store/loading/loading.selector.ts
+++
@@ -1,8 +0,0 @@
-import { createSelector } from 'reselect';
-
-const selectIsLoadingReducer = (state) => state.loading;
-
-export const selectIsLoading = createSelector(
-  [selectIsLoadingReducer],
-  (loadingSlice) => loadingSlice.isLoading,
-);
