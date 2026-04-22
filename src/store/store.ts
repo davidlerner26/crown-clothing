e239cdc353540fb9fef7913a7c64454e339c286e
David Lerner <david.lerner@hotmail.com>
2026-04-21 21:27:16 -0300

    Migrate store to RTK

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,13 @@
-import {
-  compose,
-  createStore,
-  applyMiddleware,
-  type Middleware,
-  type StoreEnhancer,
-} from 'redux';
+import { configureStore, type Middleware } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
   type PersistConfig,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
@@ -15,51 +15,39 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import { rootSaga } from './root-saga';
-
 import { rootReducer } from './root-reducer';
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-  }
-}
-
-type ExtendedPersistConfig = PersistConfig<RootState> & {
-  whitelist: (keyof RootState)[];
-};
-
-const persistConfig: ExtendedPersistConfig = {
+const persistConfig: PersistConfig<RootState> = {
   key: 'root',
   storage,
   whitelist: ['cart'],
 };
 
 const sagaMiddleware = createSagaMiddleware();
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-const middleWares: Middleware[] = [sagaMiddleware];
 
 const isDev = import.meta.env.MODE !== 'production';
 
-if (isDev) {
-  middleWares.push(logger as Middleware);
-}
+const noopMiddleware: Middleware = () => (next) => (action) => next(action);
+const loggerMiddleware = logger as unknown as Middleware;
+const devLoggerMiddleware = isDev ? loggerMiddleware : noopMiddleware;
 
-const composeEnhancer: typeof compose =
-  (isDev &&
-    typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
-
-const composedEnhancers: StoreEnhancer = composeEnhancer(
-  applyMiddleware(...middleWares),
-);
-
-export const store = createStore(persistedReducer, composedEnhancers);
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(sagaMiddleware, devLoggerMiddleware),
+  devTools: isDev,
+});
 
 sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
